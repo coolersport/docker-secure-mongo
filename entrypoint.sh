0af018e48d396d8ba87f646846f183fe6f47ff00
Tien Tran <tiendung.tran@gmail.com>
2018-01-05 14:25:50 +1100

    Added database user

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,11 @@ cp /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 MONGODB_ADMIN_USER=${MONGODB_ADMIN_USER:-"mongoadmin"}
 MONGODB_ADMIN_PASS=${MONGODB_ADMIN_PASS:-"50meP4s5w0rd"}
 
+# Application Database User
+MONGODB_APPLICATION_DATABASE=${MONGODB_APPLICATION_DATABASE:-"admin"}
+MONGODB_APPLICATION_USER=${MONGODB_APPLICATION_USER:-"dbuser"}
+MONGODB_APPLICATION_PASS=${MONGODB_APPLICATION_PASS:-"50meP4s5w0rd"}
+
 # start without authentication
 docker-entrypoint.sh mongod >/dev/null &
 
@@ -20,7 +25,13 @@ done
 # Create the admin user
 mongo admin --eval "\
     db.dropAllUsers();\
-    db.createUser({user: '$MONGODB_ADMIN_USER', pwd: '$MONGODB_ADMIN_PASS', roles:[{role:'root',db:'admin'}]});\
-    db.shutdownServer();"
+    db.createUser({user: '$MONGODB_ADMIN_USER', pwd: '$MONGODB_ADMIN_PASS', roles:[{role:'root',db:'admin'}]});"
+# Create the database and dbuser
+if [ "$MONGODB_APPLICATION_DATABASE" != "admin" ]; then
+	mongo $MONGODB_APPLICATION_DATABASE --eval "\
+	    db.createUser({user: '$MONGODB_APPLICATION_USER', pwd: '$MONGODB_APPLICATION_PASS', roles:[{role:'dbOwner', db:'$MONGODB_APPLICATION_DATABASE'}]});"
+fi
+# shut down unauth instance
+mongo admin --eval "db.shutdownServer()"
 
 exec docker-entrypoint.sh "$@"
