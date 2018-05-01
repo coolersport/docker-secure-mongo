4dba8468f553e8b43e95607edc55736d6f4bb40d
Tien Tran <tiendung.tran@gmail.com>
2018-05-01 13:59:28 +1000

    Fixed replica_key var check

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 cp /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
-if [[ ! -n "$REPLICA_KEY" ]]; then
+if [[ -n "$REPLICA_KEY" ]]; then
     echo $REPLICA_KEY > /etc/replica.key
     chmod 400 /etc/replica.key
     chown mongodb:mongodb /etc/replica.key
